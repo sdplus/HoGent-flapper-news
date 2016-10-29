ee8ff5465a3f7eb68f5acb6d3d9258d98fe8dd2a
Sofie De Plus <sofie.deplus.v7149@student.hogent.be>
2016-10-30 01:56:02 +0200

    Adding passport to application

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
+var passport = require('passport');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 mongoose.connect('mongodb://localhost/news');
 
 var express = require('express');
@@ -26,6 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
