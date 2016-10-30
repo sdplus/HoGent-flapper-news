503ee9f2dfd314107bdd3af1483e9b36a16f557d
Sofie De Plus <sofie.deplus.v7149@student.hogent.be>
2016-10-30 02:15:39 +0100

    Adding Navigation

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -240,3 +240,13 @@ app.controller('AuthCtrl', [
     };
   };
 ]);
+
+app.controller('NavCtrl', [
+  '$scope',
+  'auth',
+  function($scope, auth) {
+    $scope.isLoggedIn = auth.isLoggedIn;
+    $scope.currentUser = auth.currentUser;
+    $scope.logOut = auth.logOut;
+  }
+]);
